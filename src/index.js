05ff8679a1311354aa9cb830147bc269f10d2c58
coke12103 <coke12103@gmail.com>
2019-06-22 19:37:05 +0900

    add views

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+const electron = require('electron');
+const remote = electron.remote;
+
+function init(){
+  var confirm_button = document.getElementById("confirm");
+
+  confirm_button.addEventListener("click", get_img, false);
+}
+
+function get_img(){
+  var input_url = document.getElementById("url_input");
+  var request = remote.require('request');
+
+  request.get(input_url.value, (err, res, body) => {
+      if(err){
+        console.log('Error: ' + err.message);
+        return;
+      }
+      console.log(res);
+      console.log(body);
+  })
+}
+
+window.onload = init;
