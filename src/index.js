ca77218e0763106659d29e65fc66bd20689479e5
coke12103 <coke12103@gmail.com>
2019-06-22 20:58:41 +0900

    add image url get

--- src/index.js
+++ src/index.js
@@ -9,15 +9,30 @@ function init(){
 
 function get_img(){
   var input_url = document.getElementById("url_input");
+  var status_text = document.getElementById("status_text");
   var request = remote.require('request');
+  var html_parser = remote.require('fast-html-parser');
 
   request.get(input_url.value, (err, res, body) => {
       if(err){
         console.log('Error: ' + err.message);
         return;
       }
+
+      var parse_body = html_parser.parse(body);
+
+      status_text.innerText = "Get page: " + res.statusMessage;
+      for(var meta_tag of parse_body.querySelectorAll('meta')){
+        // video: og:video:url
+        if(meta_tag.attributes.property == "og:image"){
+          var media_url = meta_tag.attributes.content;
+          media_url = media_url.replace("large", "orig");
+          console.log(media_url);
+        }
+      }
       console.log(res);
-      console.log(body);
+
+      // console.log(body);
   })
 }
 
