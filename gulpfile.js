1a3d3eac4370198a74174c28418b41efe22f04be
coke12103 <coke12103@gmail.com>
2019-07-18 23:10:39 +0900

    add packager

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var packager = require('electron-packager');
+var config = require('./package.json');
+
+gulp.task('packager-win', (done) => {
+    packager({
+        dir: './',
+        out; './release',
+        name: config.name,
+        arch: 'x64',
+        platform: 'win32',
+        electronVersion: '5.0.5',
+        overwrite: true,
+        asar: true,
+        appVersion: config.version,
+        appCopyright: '',
+    });
+});
+
+gulp.task('packager-linux', (done) => {
+    packager({
+        dir: './',
+        out; './release',
+        name: config.name,
+        arch: 'x64',
+        platform: 'linux',
+        electronVersion: '5.0.5',
+        overwrite: true,
+        asar: true,
+        appVersion: config.version,
+        appCopyright: '',
+    });
+});
