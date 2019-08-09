6e749f0aef12b2cb59063cb7e02675f1a8cb2631
coke12103 <coke12103@gmail.com>
2019-08-09 22:46:05 +0900

    move misskey download action to lib/downloader/misskey.js

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function get_img(url){
       downloader.twitter(url, save_dir);
       break;
     case "misskey":
-      get_misskey_img(url, save_dir);
+      downloader.misskey(url, save_dir);
       break;
     case "mastodon":
       get_mastodon_img(url, save_dir);
@@ -51,63 +51,6 @@ function get_img(url){
   }
 }
 
-function get_misskey_img(input_url, save_dir){
-  var request = remote.require('request');
-  var url = remote.require('url');
-  var parse_url = url.parse(input_url);
-  var note_id = parse_url.pathname.match(/notes\/(.+)/)[1]
-
-  var domain = parse_url.protocol + "//" + parse_url.host;
-
-  var req = {
-    url: domain + '/api/notes/show',
-    method: 'POST',
-    json: {
-      "noteId": note_id
-    }
-  }
-
-  console.log(note_id)
-  request(req, async (err, res, body) => {
-      if(err){
-        console.log('Error: ' + err.message);
-        return;
-      }
-
-      if(body.uri){
-        console.log("remote")
-        set_status_text("Remote Posts.")
-        get_img(body.uri)
-        console.log(body.uri);
-
-        return;
-      }
-
-      if(!body.files){
-        set_status_text("No Image File");
-        return;
-      }
-
-      var image_count = 0;
-      for(i = 0; body.files.length > i; i++){
-        var extension = body.files[i].name.match(/\.[a-zA-Z0-9]+$/);
-        var file_name = "mk_" + body.user.username + "_";
-        if(config.file_name_domain){
-          file_name = file_name + parse_url.host + "_";
-        }
-        file_name = file_name + body.id + "_image" + image_count + extension;
-
-        try{
-          await get_image_file(body.files[i].url, file_name, save_dir);
-        }catch{
-          notification.error_notification("ファイルの書き込みに失敗しました!\nHint: 保存先に指定されたフォルダが消えていませんか？消えていないならそのフォルダに書き込み権限はありますか？");
-          return;
-        }
-        image_count++
-      }
-      notification.end_notification(image_count, save_dir + '/' + file_name);
-  })
-}
 
 function get_mastodon_img(input_url, save_dir){
   var request = remote.require('request');
